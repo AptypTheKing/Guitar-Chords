69c9f913523f56f37febd04efe130f7aab0d3952
Artur Burlak <aptyp1the1king@gmail.com>
2022-08-15 18:51:08 +0300

    Feat: Show all types of chords with scale and pitch options

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-let content
+let load
 let showed = false
 
-let chords = document.querySelector('.chords')
+let guitar = document.querySelector('.guitar')
 
-chords.addEventListener('click', function(event) { // handle the click on button, change button class
+let chords = document.querySelector('.chords')
+let options = document.querySelector('.options')
+let scale = options.querySelector('.scale')
+let alt = options.querySelector('.alt')
+let flat = alt.querySelector('#flat')
+let sharp = alt.querySelector('#sharp')
+let maj = scale.querySelector('#maj')
+let min = scale.querySelector('#min')
+
+let currentNote = ''
+let currentScale = 'maj'
+let currentAlt = ''
+
+chords.addEventListener('click', function(event) { // handle the click on chord
   let target = event.target
   let active = document.querySelector('.showed')
 
   if (target.tagName != 'BUTTON') return
+  currentNote = target.innerHTML
   if (active) {
     active.classList.remove('showed')
     target.classList.toggle('showed')
-    showChord(target)
   }
   if (active === target) {
     active.classList.remove('showed')
-    showChord()
+    currentNote = ''
   } else {
     target.classList.add('showed')
-    showChord(target)
   }
+  showChord()
 })
 
 function loadChord(letter) { // load and locate the chords
@@ -33,21 +46,113 @@ function loadChord(letter) { // load and locate the chords
                                 
   div.style.position = 'relative';
   div.style.top =  -guitarCoords.height + 'px'
+  div.style.zIndex = '1'
+
   return div
 }
 
-function showChord(content) { // show the chord
-  let guitar = document.querySelector('.guitar')
+function showChord() { // show the chord
   if (showed) {
     guitar.lastChild.remove()
     showed = false
-  } 
-                
-  if (!content) return
+  }
 
-  let load = loadChord(content.innerHTML)
+  for (let btn of alt.querySelectorAll('.btn')) {
+    if (btn.hasAttribute('disabled')) {
+      btn.removeAttribute('disabled')
+    }
+  }
+
+  if (!currentNote) {
+    clear()
+    return
+  }
+
+  if (currentNote === 'H' || currentNote === 'E') {
+    sharp.setAttribute('disabled', 'disabled')
+    sharp.classList.remove('showed')
+    if (currentAlt === 'sharp') {
+      currentAlt = ''
+    }
+  } else if (currentNote === 'C' || currentNote === 'F') {
+    flat.setAttribute('disabled', 'disabled')
+    flat.classList.remove('showed')
+    if (currentAlt === 'flat') {
+      currentAlt = ''
+    }
+  }
+
+  load = loadChord(currentNote + currentAlt + currentScale)
   document.body.style.overflow = 'hidden'
   guitar.append(load)
   showed = true
+
+  showScale()
 }
+
+function clear() { // clear all selected chords and options
+  let buttons = document.querySelector('.buttons')
+  for (let btn of buttons.querySelectorAll('.btn')) {
+    btn.classList.remove('showed')
+  }
+  currentAlt = ''
+}
+
+function showScale() {  // set and show default scale 'Maj'
+  
+  if (currentScale === 'maj' || !currentScale) {
+    maj.classList.add('showed')
+  } else {
+    min.classList.add('showed')
+  }
+  return
+ 
+}
+
+scale.addEventListener('click', function(event) { // handle the click on Scale
+  let target = event.target
+  let active = scale.querySelector('.showed')
+
+  if (target.tagName != 'BUTTON') return
+
+  if (target === active) return
+
+  if (!active) {
+    target.classList.add('showed')
+    currentScale = target.innerHTML.toLowerCase()
+    return
+  }
+
+  active.classList.remove('showed')
+  target.classList.add('showed')
+  currentScale = target.innerHTML.toLowerCase()
+  showChord()
+  
+})
+
+alt.addEventListener('click', function(event) { // handle the click on Alt
+  let target = event.target
+  let active = alt.querySelector('.showed')
+
+  if (target.tagName != 'BUTTON') return
+  if (target === active) {
+    target.classList.remove('showed')
+    currentAlt = ''
+    showChord()
+    return
+  }
+
+  if (!active) {
+    target.classList.add('showed')
+    currentAlt = target.id
+    showChord()
+    return
+  }
+
+  active.classList.remove('showed')
+  target.classList.add('showed')
+  currentAlt = target.id
+  showChord()
+})
+
             
